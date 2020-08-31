c0450b2ad54c003e714cff37f7ecb1e73bb7f97c
O'Brien Mark <O'BrienMarkJ@JohnDeere.com>
2020-08-31 11:00:08 -0500

    adding javascript

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,16 +3,27 @@ console.log("Up and running!");
 
 let cards = ["queen","queen","king", "king"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[1];
+//let cardOne = cards[0];
+//let cardTwo = cards[1];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+//cardsInPlay.push(cardOne);
+//cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+	
+function flipCard (cardId)
+{
+	console.log("Flipped "+ cards[cardId] );
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+	
+}
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1] )
-	alert("You found a match");
-else if (cardsInPlay.length === 2)
-	alert("Cards don't match");
+function checkForMatch()
+{
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1] )
+		alert("You found a match");
+	else if (cardsInPlay.length === 2)
+		alert("Cards don't match");
+}
+flipCard(0);
+flipCard(2);
